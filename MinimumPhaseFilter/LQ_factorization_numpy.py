181aa427cd911ede0b9544e29d1f45d65c779e39
Павел Кривенко <pavelkr@MacBook-Pro-Pavel.local>
2026-02-28 23:58:53 +0300

    more simple QL factorization

---
+++ MinimumPhaseFilter/LQ_factorization_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.linalg import qr
+
+def LQFactorization(H: np.array) -> tuple:
+    H_transp = H.T
+    Q_transp, R = qr(H_transp)
+    L = R.T
+    Q = Q_transp.T
+
+    return L, Q
--- MinimumPhaseFilter/test_QL.py
+++ MinimumPhaseFilter/test_QL.py
@@ -1,21 +1,25 @@
-# import pytest
 import numpy as np
-from QL_decomposition import QL_factorize
+from LQ_factorization_numpy import LQFactorization
 
 def test_QL():
     np.random.seed(42)
     M, N = 3, 3
     B = np.array([[1, 2, 3], [2, 4, 6], [4, 8, 12]])
-    L, Q = QL_factorize(B)
+    L, Q = LQFactorization(B)
+
+    print('B = ', B)
+    print('----------------------------------------------------------')
+    print('L = ', L)
+    print('----------------------------------------------------------')
+    print('Q = ', Q)
+    print('----------------------------------------------------------')
+    print('LQ = ', L @ Q)
     
-    # Проверка размеров
     assert Q.shape == (M, M)
     assert L.shape == (M, N)
 
-    # Проверка: B = Q * L
-    assert np.allclose(B, Q @ L, atol=1e-10)
+    assert np.allclose(B, L @ Q, atol=1e-10)
 
-    # Проверка, что Q - ортогональная 
     assert np.allclose(Q @ Q.T, np.eye(M), atol=1e-10)
     assert np.allclose(Q.T @ Q, np.eye(M), atol=1e-10)
 
