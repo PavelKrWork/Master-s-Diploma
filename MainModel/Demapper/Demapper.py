8206142b71b7bba8dd65ecb699950f4a06697f24
Павел Кривенко <pavelkr@MacBook-Pro-Pavel.local>
2026-03-10 02:13:57 +0300

    Demapper hard decision

--- MainModel/Demapper/Demapper.py
+++ MainModel/Demapper/Demapper.py
@@ -1,18 +1,31 @@
-class DemapperИase:
-    def __init__(self, constellation_type, bits_per_symbol):
-        pass
-   
-    def _generate_constellation(self):
-        pass
-    
-    def _generate_mapping(self):
-        pass
+import numpy as np
+class DemapperBase:
+    def __init__(self, constellation):
+        self.constellation = constellation
+        self.points = constellation.points
+        self.bits_per_symbol = constellation.bits_per_symbol
+        self.idx_to_bits = constellation.idx_to_bits
+        self.iq_points_num = len(self.points)
 
+class HardDemapper(DemapperBase):
+    # поиск наименьшего расстояния от исследуемой точки в received_signal до точек созвездия
+    def demodulate(self, received_signal) -> list:
+        n_symbols = len(received_signal)
+        res_bits = [0] * n_symbols * self.bits_per_symbol
+        for ind in range(n_symbols):
+            rx_iq_i = received_signal[ind]
+            min_dist = float('inf')
+            selec_idx = 0
+            for idx, const_iq_j in enumerate(self.points):
+                dist = np.abs(rx_iq_i - const_iq_j)
+                if dist < min_dist:
+                    min_dist = dist
+                    selec_idx = idx
+            
+            res_bits[ind * self.bits_per_symbol : (ind + 1) * self.bits_per_symbol] = self.idx_to_bits[selec_idx]
 
-class HardDemapper(Demapper):
-    def demodulate(self, received_signal):
-        pass
+        return res_bits
 
-class SoftDemapper(Demapper):
-    def demodulate(self, received_signal, noise_variance):
+class SoftDemapper(DemapperBase):
+    def demodulate(self, received_signal, noise_variance) -> list:
         pass
--- Tests/test_Demapper.py
+++ Tests/test_Demapper.py
@@ -3,16 +3,22 @@ sys.path.insert(1, '../Master-s-Diploma/MainModel')
 
 
 from Mapper.Mapper import Mapper
+from Demapper.Demapper import HardDemapper
 from Constellation.Constellation import Constellation
 import numpy as np
 
 test_bits = [1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0]
+
+print("Initial bits: \n", test_bits)
 
 const_bpsk = Constellation(modulation_type="bpsk")
 print("BPSK IQ points: ", const_bpsk.points)
 mapper = Mapper(const_bpsk)
 symbols_bpsk = mapper.modulate(test_bits)
 print("BPSK modulation: \n", symbols_bpsk)
+demapper = HardDemapper(const_bpsk)
+demod_bpsk_bits = demapper.demodulate(symbols_bpsk)
+print("BPSK demodulated bits: \n", demod_bpsk_bits)
 
 print()
 
@@ -21,6 +27,9 @@ print("QPSK IQ points: ", const_qpsk.points)
 mapper = Mapper(const_qpsk)
 symbols_qpsk = mapper.modulate(test_bits)
 print("QPSK modulation: \n", symbols_qpsk)
+demapper = HardDemapper(const_qpsk)
+demod_qpsk_bits = demapper.demodulate(symbols_qpsk)
+print("BPSK demodulated bits: \n", demod_qpsk_bits)
 
 print()
 
@@ -29,6 +38,9 @@ print("16QAM IQ points: ", const_16qam.points)
 mapper = Mapper(const_16qam)
 symbols_16qam = mapper.modulate(test_bits)
 print("16QAM modulation: \n", symbols_16qam)
+demapper = HardDemapper(const_16qam)
+demod_16qam_bits = demapper.demodulate(symbols_16qam)
+print("BPSK demodulated bits: \n", demod_16qam_bits)
 
 print()
 
@@ -37,3 +49,6 @@ print("64QAM IQ points: ", const_64qam.points)
 mapper = Mapper(const_64qam)
 symbols_64qam = mapper.modulate(test_bits)
 print("64QAM modulation: \n", symbols_64qam)
+demapper = HardDemapper(const_64qam)
+demod_64qam_bits = demapper.demodulate(symbols_64qam)
+print("BPSK demodulated bits: \n", demod_64qam_bits)
